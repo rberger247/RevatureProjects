fcab9acafb6b91e764707cc90df7a0df4a12a772
rafael berger <Rafiberger613@gmail.com>
2018-04-11 14:40:04 -0400

    Added Search Functionality

--- Restaraunt3/Program.cs
+++ Restaraunt3/Program.cs
@@ -16,6 +16,7 @@ namespace Restaraunt3
                 Name = "Bobs Burgers",
                 Ratings = RatingsList
             };
+            Search();
          Console.WriteLine(  GetAverageRatings(restaraunt));
             Console.ReadLine();
 
@@ -28,6 +29,36 @@ namespace Restaraunt3
 
             return listOfRestaraunts;
 
+        }
+
+        public static List<Restaurants> Search()
+        {
+          var  userInput = "";
+            List<Restaurants> restarauntList = new List<Restaurants> {  };
+            List<double> RatingsList = new List<double>();
+
+            Restaurants restaraunt1 = new Restaurants()
+            {
+                Name = "Bobs Burgers",
+                Ratings = RatingsList
+            };
+            Restaurants restaraunt2 = new Restaurants()
+            {
+                Name = "Harry Burgers",
+                Ratings = RatingsList
+            };
+            restarauntList.Add(restaraunt1);
+            restarauntList.Add(restaraunt2);
+
+            var searchedList =  restarauntList.Select(m => new Restaurants
+            {
+      
+                Name = m.Name
+            })
+           .Where(m => string.IsNullOrEmpty(userInput) || m.Name.StartsWith(userInput)).ToList();
+
+
+            return searchedList;
         }
         public static double GetAverageRatings(Restaurants restaurants)
         {
