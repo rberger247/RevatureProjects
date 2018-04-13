03118292c3b9a6e9c7a877c8abf111d968bd38a2
rafael berger <Rafiberger613@gmail.com>
2018-04-13 12:34:21 -0400

    Added Exception Handling

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace ExceptionHandling
 {
@@ -10,6 +11,41 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
+     
+            string[] answer = new string[3];
+
+
+            try
+            {
+                Console.WriteLine("Please Enter index of array you you would like to read");
+                var userInput = Console.Read();
+
+                Console.WriteLine(answer[userInput]);
+                Console.Read();
+
+            }
+
+
+            catch (Exception ex)
+            {
+
+                //Logger logger = LogManager.GetCurrentClassLogger();
+                //logger.Debug("Hello World!");
+                Logger logger = LogManager.GetLogger("databaseLogger");
+                logger.Error(ex);
+
+                Console.WriteLine(ex.Message);
+
+
+                Console.ReadLine();
+
+            }
+            finally {
+
+                Console.ReadLine();
+            }
+
+        
         }
     }
 }
